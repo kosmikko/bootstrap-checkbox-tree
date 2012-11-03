edf8bf86ce734fc1f9a216ab5b0e2f50d1c4e9d0
Mikko Lehtinen <mikko@netcycler.com>
2012-11-03 18:55:34 +0200

    Switched to object literal notation

--- js/bootstrap-checkbox-tree.js
+++ js/bootstrap-checkbox-tree.js
@@ -1,3 +1,14 @@
+/*
+ * CheckboxTree
+ * https://github.com/mikkolehtinen/bootstrap-checkbox-tree
+ *
+ *
+ * Based on https://github.com/antihero/Collapsible-Checkbox-Tree-jQuery-Plugin
+ *
+ * Copyright (c) 2012 Mikko Lehtinen
+ * Licensed under the MIT license.
+ */
+
 !function ($) {
   "use strict";
 
@@ -24,108 +35,111 @@
     return this;
   };
 
-  CheckboxTree.prototype.initMarkup = function() {
-    // Add tree collapse/expand links
-    this.$element.find("li").prepend('<span>&nbsp;</span>');
-  };
+  CheckboxTree.prototype = {
 
-  CheckboxTree.prototype.checkboxTicked = function(e) {
-    var $cb = $(e.currentTarget);
+    initMarkup: function() {
+      // Add tree collapse/expand links
+      this.$element.find("li").prepend('<span>&nbsp;</span>');
+    },
 
-    if ($cb.is(":checked")) {
-      // Show immediate children  of checked
-      $cb.parent("li").find("> ul").removeClass('hide');
-      // Update the tree
-      this.expandEl($cb.parent("li").find("> span.collapsed"));
+    checkboxTicked: function(e) {
+      var $cb = $(e.currentTarget);
+
+      if ($cb.is(":checked")) {
+        // Show immediate children  of checked
+        $cb.parent("li").find("> ul").removeClass('hide');
+        // Update the tree
+        this.expandEl($cb.parent("li").find("> span.collapsed"));
+
+        // Check parents if necessary
+        if (this.options.checkParents) {
+          $cb.parents("li").find("input[type='checkbox']:first").attr('checked', true);
+        }
+
+        // Check children if necessary
+        if (this.options.checkChildren) {
+          $cb.parent("li").find("input[type='checkbox']").attr('checked', true);
+          // Show all children of checked
+          $("ul", $cb.parent("li")).removeClass('hide');
+          // Update the tree
+          this.expandEl($("span.collapsed", $cb.parent("li")));
+        }
+
+      // If unchecking...
+      } else {
+
+        // Uncheck children if necessary
+        if (this.options.uncheckChildren) {
+          $cb.parent("li").find("input[type='checkbox']").attr('checked', false);
+          // Hide all children
+          if(this.options.hideChildrenWhenUnchecking) {
+            $("ul", $cb.parent("li")).addClass('hide');
+            // Update the tree
+            this.collapseEl($("span.expanded", $(this).parent("li")));
+          }
+        }
+      }
+    },
+
+    expandOrCollapseBranch: function(e) {
+      var $el = $(e.currentTarget);
+      // If was previously collapsed...
+      if ($el.is(".collapsed")) {
+
+        // ... then expand
+        $("> ul", $el.parent("li")).removeClass('hide');
+        this.expandEl($el);
+
+      // If was previously expanded...
+      } else if ($el.is(".expanded")) {
+
+        // ... then collapse
+        $("> ul", $el.parent("li")).addClass('hide');
+        this.collapseEl($el);
+      }
+    },
+
+    expandAll: function() {
+      // Show all children
+      this.$element.find("ul").removeClass('hide');
+      // and update the html
+      this.expandEl(this.$element.find("li:has(> ul) > span"));
+    },
+
+    collapseAll: function() {
+      // Hide all children
+      this.$element.find("ul").addClass('hide');
+      // and update the html
+      this.collapseEl(this.$element.find("li:has(> ul) > span"));
+    },
+
+    defaultExpand: function() {
+      // Hide all except top level
+      this.$element.find("ul").addClass('hide');
+      // Show checked and immediate children of checked
+      this.$element.find("li:has(input:checked) > ul").removeClass('hide');
 
       // Check parents if necessary
       if (this.options.checkParents) {
-        $cb.parents("li").find("input[type='checkbox']:first").attr('checked', true);
+        this.$element.find("input:checked").parents("li").find("input[type='checkbox']:first").attr('checked', true);
       }
-
       // Check children if necessary
       if (this.options.checkChildren) {
-        $cb.parent("li").find("input[type='checkbox']").attr('checked', true);
-        // Show all children of checked
-        $("ul", $cb.parent("li")).removeClass('hide');
-        // Update the tree
-        this.expandEl($("span.collapsed", $cb.parent("li")));
+        this.$element.find("input:checked").parent("li").find("input[type='checkbox']").attr('checked', true);
       }
 
-    // If unchecking...
-    } else {
+      // and update the html
+      this.expandEl(this.$element.find("li:has(> ul:not(.hide)) > span"));
+      this.collapseEl(this.$element.find("li:has(> ul.hide) > span"));
+    },
 
-      // Uncheck children if necessary
-      if (this.options.uncheckChildren) {
-        $cb.parent("li").find("input[type='checkbox']").attr('checked', false);
-        // Hide all children
-        if(this.options.hideChildrenWhenUnchecking) {
-          $("ul", $cb.parent("li")).addClass('hide');
-          // Update the tree
-          this.collapseEl($("span.expanded", $(this).parent("li")));
-        }
-      }
+    expandEl: function($el) {
+      $el.removeClass("collapsed").addClass("expanded").html(this.options.closeHtml);
+    },
+
+    collapseEl: function($el) {
+      $el.removeClass("expanded").addClass("collapsed").html(this.options.openHtml);
     }
-  };
-
-  CheckboxTree.prototype.expandOrCollapseBranch = function(e) {
-    var $el = $(e.currentTarget);
-    // If was previously collapsed...
-    if ($el.is(".collapsed")) {
-
-      // ... then expand
-      $("> ul", $el.parent("li")).removeClass('hide');
-      this.expandEl($el);
-
-    // If was previously expanded...
-    } else if ($el.is(".expanded")) {
-
-      // ... then collapse
-      $("> ul", $el.parent("li")).addClass('hide');
-      this.collapseEl($el);
-    }
-  };
-
-  CheckboxTree.prototype.expandAll = function() {
-    // Show all children
-    this.$element.find("ul").removeClass('hide');
-    // and update the html
-    this.expandEl(this.$element.find("li:has(> ul) > span"));
-  };
-
-  CheckboxTree.prototype.collapseAll = function() {
-    // Hide all children
-    this.$element.find("ul").addClass('hide');
-    // and update the html
-    this.collapseEl(this.$element.find("li:has(> ul) > span"));
-  };
-
-  CheckboxTree.prototype.defaultExpand = function() {
-    // Hide all except top level
-    this.$element.find("ul").addClass('hide');
-    // Show checked and immediate children of checked
-    this.$element.find("li:has(input:checked) > ul").removeClass('hide');
-
-    // Check parents if necessary
-    if (this.options.checkParents) {
-      this.$element.find("input:checked").parents("li").find("input[type='checkbox']:first").attr('checked', true);
-    }
-    // Check children if necessary
-    if (this.options.checkChildren) {
-      this.$element.find("input:checked").parent("li").find("input[type='checkbox']").attr('checked', true);
-    }
-
-    // and update the html
-    this.expandEl(this.$element.find("li:has(> ul:not(.hide)) > span"));
-    this.collapseEl(this.$element.find("li:has(> ul.hide) > span"));
-  };
-
-  CheckboxTree.prototype.expandEl = function($el) {
-    $el.removeClass("collapsed").addClass("expanded").html(this.options.closeHtml);
-  };
-
-  CheckboxTree.prototype.collapseEl = function($el) {
-    $el.removeClass("expanded").addClass("collapsed").html(this.options.openHtml);
   };
 
   $.fn.checkboxTree = function (options) {
@@ -137,7 +151,8 @@
     onBranchExpand: function () {},
     openHtml: '&#9658;', // Html for open icon
     closeHtml: '&#9660;', // close icon
-    checkChildren : false, // When checking a box, all children are checked
+    checkParents : false, // When checking a box, all parents are checked
+    checkChildren : true, // When checking a box, all children are checked
     uncheckChildren : true, // When unchecking a box, all children are unchecked
     initialState : 'default' // Options - 'expand' (fully expanded), 'collapse' (fully collapsed) or default
   };
